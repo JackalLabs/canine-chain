4ef4fbbb414bf0dc891cacc5477fae4775cd0b43
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-06-20 19:09:31 -0400

    cleaned cli tool queries

--- x/jklmining/client/cli/query.go
+++ x/jklmining/client/cli/query.go
@@ -31,12 +31,7 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	cmd.AddCommand(CmdShowMiners())
 	cmd.AddCommand(CmdListMined())
 	cmd.AddCommand(CmdShowMined())
-	cmd.AddCommand(CmdCheckMinerIndex())
-
-	cmd.AddCommand(CmdGetMinerIndex())
-
 	cmd.AddCommand(CmdGetMinerStart())
-
 	cmd.AddCommand(CmdListMinerClaims())
 	cmd.AddCommand(CmdShowMinerClaims())
 	// this line is used by starport scaffolding # 1
