f8729cfcb5e8b32e1566f649db8bc361ce2a1da6
BiPhan4 <biphan374@gmail.com>
2022-11-09 16:47:44 -0500

    tested set provider ip

--- x/storage/keeper/msg_server_providers_test.go
+++ x/storage/keeper/msg_server_providers_test.go
@@ -25,11 +25,11 @@ func (suite *KeeperTestSuite) TestMsgInitProvider() {
 			preRun: func() *types.MsgInitProvider {
 				return types.NewMsgInitProvider(
 					user.String(),
-					"127",
-					"10000",
+					"127.0.0.1",
+					"1000000000",
 				)
 			},
-			expErr: true,
+			expErr: false,
 			name:   "init provider success",
 		},
 	}
@@ -45,6 +45,49 @@ func (suite *KeeperTestSuite) TestMsgInitProvider() {
 			} else {
 				suite.Require().NoError(err)
 				suite.Require().EqualValues(types.MsgInitProviderResponse{}, *res)
+
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestMsgSetProviderIP() {
+	suite.SetupSuite()
+
+	msgSrvr, _, context := setupMsgServer(suite)
+
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	cases := []struct {
+		preRun    func() *types.MsgSetProviderIP
+		expErr    bool
+		expErrMsg string
+		name      string
+	}{
+		{
+			preRun: func() *types.MsgSetProviderIP {
+				return types.NewMsgSetProviderIP(
+					user.String(),
+					"127.0.0.1",
+				)
+			},
+			expErr: false,
+			name:   "set provider ip success",
+		},
+	}
+
+	for _, tc := range cases {
+		suite.Run(tc.name, func() {
+			msg := tc.preRun()
+			suite.Require().NoError(err)
+			res, err := msgSrvr.SetProviderIP(context, msg)
+			if tc.expErr {
+				suite.Require().Error(err)
+				suite.Require().Contains(err.Error(), tc.expErrMsg)
+			} else {
+				suite.Require().NoError(err)
+				suite.Require().EqualValues(types.MsgSetProviderIPResponse{}, *res)
 
 			}
 		})
