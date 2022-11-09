92bae39cdf289b5f4867bf3b388b94bb46b128ad
BiPhan4 <biphan374@gmail.com>
2022-11-08 19:04:56 -0500

    add keeper_test.go

---
+++ x/storage/keeper/keeper_test.go
@@ -0,0 +1,58 @@
+package keeper_test
+
+import (
+	gocontext "context"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	storage "github.com/jackalLabs/canine-chain/x/storage"
+	"github.com/jackalLabs/canine-chain/x/storage/keeper"
+	storagetestutil "github.com/jackalLabs/canine-chain/x/storage/testutil"
+	"github.com/jackalLabs/canine-chain/x/storage/types"
+	"github.com/stretchr/testify/suite"
+)
+
+type KeeperTestSuite struct {
+	suite.Suite
+
+	cdc           codec.Codec
+	ctx           sdk.Context
+	storageKeeper *keeper.Keeper
+	bankKeeper    *storagetestutil.MockBankKeeper
+	queryClient   types.QueryClient
+	msgSrvr       types.MsgServer
+}
+
+func (suite *KeeperTestSuite) SetupSuite() {
+	suite.reset()
+}
+
+func (suite *KeeperTestSuite) reset() {
+	storageKeeper, bankKeeper, encCfg, ctx := setupStorageKeeper(suite.T())
+
+	queryHelper := baseapp.NewQueryServerTestHelper(ctx, encCfg.InterfaceRegistry)
+	types.RegisterQueryServer(queryHelper, storageKeeper)
+	queryClient := types.NewQueryClient(queryHelper)
+
+	coins := sdk.NewCoins(sdk.NewCoin("stake", sdk.NewInt(1000000000)))
+	err := bankKeeper.MintCoins(ctx, minttypes.ModuleName, coins)
+	suite.NoError(err)
+	err = bankKeeper.SendCoinsFromModuleToModule(ctx, minttypes.ModuleName, types.ModuleName, coins)
+	suite.NoError(err)
+
+	suite.ctx = ctx
+	suite.storageKeeper = storageKeeper
+	suite.bankKeeper = bankKeeper
+	suite.cdc = encCfg.Codec
+	suite.queryClient = queryClient
+	suite.msgSrvr = keeper.NewMsgServerImpl(*suite.storageKeeper)
+}
+
+func setupMsgServer(suite *KeeperTestSuite) (types.MsgServer, keeper.Keeper, gocontext.Context) {
+	k := suite.storageKeeper
+	storage.InitGenesis(suite.ctx, *k, *types.DefaultGenesis())
+	ctx := sdk.WrapSDKContext(suite.ctx)
+	return keeper.NewMsgServerImpl(*k), *k, ctx
+}
