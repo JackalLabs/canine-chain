9e201a0a6da9555b460dfd247997b0257ef233cc
Nugget <nuggetnchill@gmail.com>
2022-11-11 12:45:36 -0600

    added first test case

--- x/storage/keeper/msg_server_proofs_test.go
+++ x/storage/keeper/msg_server_proofs_test.go
@@ -1,11 +1,90 @@
 package keeper_test
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/jackalLabs/canine-chain/x/storage/types"
+	merkletree "github.com/wealdtech/go-merkletree"
 )
+
+const CID = "6ef1cf960c0b1e257049645ff13ed890c2d4ef69c62165bf4e090ec480770d67"
+
+// filename := []byte(fmt.Sprintf("%s%s", "FILE-", CID))
+
+func CreateMerkleForProof() (string, string, error) {
+	// example file
+	f := []byte("jackal maxi")
+	index := 0
+	var data [][]byte
+	var item []byte = f
+
+	h := sha256.New()
+	io.WriteString(h, fmt.Sprintf("%d%x", index, f))
+	hashName := h.Sum(nil)
+
+	data = append(data, hashName)
+
+	tree, err := merkletree.New(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	h = sha256.New()
+	io.WriteString(h, fmt.Sprintf("%d%x", index, item))
+	ditem := h.Sum(nil)
+
+	proof, err := tree.GenerateProof(ditem)
+	if err != nil {
+		return "", "", err
+	}
+
+	jproof, err := json.Marshal(*proof)
+	if err != nil {
+		return "", "", err
+	}
+
+	e := hex.EncodeToString(tree.Root())
+
+	k, _ := hex.DecodeString(e)
+
+	verified, err := merkletree.VerifyProof(ditem, proof, k)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	if !verified {
+		fmt.Printf("%s\n", "Cannot verify")
+	}
+
+	return fmt.Sprintf("%x", item), string(jproof), nil
+}
+
+func makeContract() (string, string) {
+	// example file
+	f := []byte("jackal maxi")
+	size := 0
+	var list [][]byte
+
+	size = size + len(f)
+
+	h := sha256.New()
+	io.WriteString(h, fmt.Sprintf("%d%x", 0, f))
+	hashName := h.Sum(nil)
+
+	list = append(list, hashName)
+
+	t, err := merkletree.New(list)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	return hex.EncodeToString(t.Root()), fmt.Sprintf("%d", size)
+}
 
 func (suite *KeeperTestSuite) TestPostProof() {
 	suite.SetupSuite()
@@ -18,7 +97,7 @@ func (suite *KeeperTestSuite) TestPostProof() {
 	testProvider, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
 	suite.Require().NoError(err)
 
-	msgSrvr, _, context := setupMsgServer(suite)
+	msgSrvr, keeper, context := setupMsgServer(suite)
 
 	// Init Provider
 	_, err = msgSrvr.InitProvider(context, &types.MsgInitProvider{
@@ -30,16 +109,19 @@ func (suite *KeeperTestSuite) TestPostProof() {
 		fmt.Println(err)
 	}
 
-	const CID = "6ef1cf960c0b1e257049645ff13ed890c2d4ef69c62165bf4e090ec480770d67"
+	// run makeContract
+	merkleroot, filesize := makeContract()
+	// fmt.Println(merkleroot)
+	// fmt.Println(filesize)
 
 	// Post Contract
 	_, err = msgSrvr.PostContract(context, &types.MsgPostContract{
 		Creator:  testProvider.String(),
 		Signee:   user.String(),
 		Duration: "1",
-		Filesize: "1_000",
+		Filesize: filesize,
 		Fid:      "fid",
-		Merkle:   "merkle",
+		Merkle:   merkleroot,
 	})
 	if err != nil {
 		fmt.Println(err)
@@ -54,33 +136,37 @@ func (suite *KeeperTestSuite) TestPostProof() {
 		fmt.Println(err)
 	}
 
-	// item, hashlist, err :=
+	item, hashlist, err := CreateMerkleForProof()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	// cases := []struct {
-	// 	testName  string
-	// 	msg       types.MsgPostproof
-	// 	expErr    bool
-	// 	expErrMsg string
-	// }{
-	// 	{
-	// 		testName: "create 1 proof",
-	// 		msg: types.MsgPostproof{
-	// 			Creator:  testProvider.String(),
-	// 			Cid:      CID,
-	// 			Item:     "this is item",
-	// 			Hashlist: "put hashlist here",
-	// 		},
-	// 		expErr:    false,
-	// 		expErrMsg: "",
-	// 	},
-	// }
+	cases := []struct {
+		testName  string
+		msg       types.MsgPostproof
+		expErr    bool
+		expErrMsg string
+	}{
+		{
+			testName: "create 1 proof",
+			msg: types.MsgPostproof{
+				Creator:  testProvider.String(),
+				Cid:      CID,
+				Item:     item,
+				Hashlist: hashlist,
+			},
+			expErr:    false,
+			expErrMsg: "",
+		},
+	}
 
-	// for _, tc := range cases {
-	// 	suite.Run(
-	// 		tc.testName, func() {
-	// 			_, err := msgSrvr.Postproof(context, &tc.msg)
-	// 			fmt.Println(err)
-	// 		},
-	// 	)
-	// }
+	for _, tc := range cases {
+		suite.Run(
+			tc.testName, func() {
+				_, err := msgSrvr.Postproof(context, &tc.msg)
+				fmt.Println(err)
+			},
+		)
+		fmt.Println(keeper.GetAllActiveDeals(suite.ctx)[0].Proofverified)
+	}
 }
