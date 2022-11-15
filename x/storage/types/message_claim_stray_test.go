189c72f6d1c958fb5d4a96d0fc0d64ea447bd6c0
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-11-15 14:10:35 -0500

    claim stray test

--- x/storage/types/message_claim_stray_test.go
+++ x/storage/types/message_claim_stray_test.go
@@ -17,12 +17,21 @@ func TestMsgClaimStray_ValidateBasic(t *testing.T) {
 			name: "invalid address",
 			msg: MsgClaimStray{
 				Creator: "invalid_address",
+				Cid:     "jklc1j3p63s42w7ywaczlju626st55mzu5z39qh6g4g",
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
+			name: "invalid cid",
+			msg: MsgClaimStray{
+				Creator: "jkl1j3p63s42w7ywaczlju626st55mzu5z399f5n6n",
+				Cid:     "invalid_cid",
+			},
+			err: sdkerrors.ErrInvalidRequest,
+		}, {
 			name: "valid address",
-			msg:  MsgClaimStray{
-				// Creator: sample.AccAddress(),
+			msg: MsgClaimStray{
+				Creator: "jkl1j3p63s42w7ywaczlju626st55mzu5z399f5n6n",
+				Cid:     "jklc1j3p63s42w7ywaczlju626st55mzu5z39qh6g4g",
 			},
 		},
 	}
