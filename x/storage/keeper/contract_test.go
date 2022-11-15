b46d1e4f039aabe5360eb71b9952d61b9031dd1a
BiPhan4 <biphan374@gmail.com>
2022-11-14 20:46:26 -0500

    tested contracts.go

--- x/storage/keeper/contract_test.go
+++ x/storage/keeper/contract_test.go
@@ -7,6 +7,149 @@ import (
 
 //testing: contracts.go active_deals.go...
 
+func (suite *KeeperTestSuite) TestSetContracts() {
+	suite.SetupSuite()
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
+	suite.Require().NoError(err)
+
+	contract := types.Contracts{
+		Cid:      "549",
+		Merkle:   "",
+		Signee:   user.String(),
+		Duration: "1000",
+		Filesize: "5000",
+		Fid:      "5789",
+		Creator:  provider.String(),
+	}
+
+	suite.storageKeeper.SetContracts(suite.ctx, contract)
+	suite.Require().NoError(err)
+
+	contractRequest := types.QueryGetContractsRequest{
+		Cid: "549",
+	}
+
+	res, err := suite.queryClient.Contracts(suite.ctx.Context(), &contractRequest)
+	suite.Require().NoError(err)
+	suite.Require().Equal(res.Contracts.Cid, contract.Cid)
+	suite.Require().Equal(res.Contracts.Signee, contract.Signee)
+
+}
+
+func (suite *KeeperTestSuite) TestGetContracts() {
+	suite.SetupSuite()
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
+	suite.Require().NoError(err)
+
+	contract := types.Contracts{
+		Cid:      "549",
+		Merkle:   "",
+		Signee:   user.String(),
+		Duration: "1000",
+		Filesize: "5000",
+		Fid:      "5789",
+		Creator:  provider.String(),
+	}
+
+	suite.storageKeeper.SetContracts(suite.ctx, contract)
+	suite.Require().NoError(err)
+
+	foundContract, found := suite.storageKeeper.GetContracts(suite.ctx, "549")
+
+	suite.Require().NoError(err)
+	suite.Require().Equal(found, true)
+	suite.Require().Equal(foundContract.Cid, contract.Cid)
+	suite.Require().Equal(foundContract.Signee, contract.Signee)
+
+}
+
+func (suite *KeeperTestSuite) TestGetAllContracts() {
+	suite.SetupSuite()
+
+	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
+	suite.Require().NoError(err)
+
+	alice, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	charlie, err := sdk.AccAddressFromBech32("cosmos1xetrp5dwjplsn4lev5r2cu8en5qsq824vza9nu")
+	suite.Require().NoError(err)
+
+	contract := types.Contracts{
+		Cid:      "549",
+		Merkle:   "",
+		Signee:   alice.String(),
+		Duration: "1000",
+		Filesize: "5000",
+		Fid:      "5789",
+		Creator:  provider.String(),
+	}
+
+	suite.storageKeeper.SetContracts(suite.ctx, contract)
+	suite.Require().NoError(err)
+
+	contract1 := types.Contracts{
+		Cid:      "649",
+		Merkle:   "",
+		Signee:   charlie.String(),
+		Duration: "2000",
+		Filesize: "10000",
+		Fid:      "4587",
+		Creator:  provider.String(),
+	}
+
+	suite.storageKeeper.SetContracts(suite.ctx, contract1)
+	suite.Require().NoError(err)
+
+	allContracts := suite.storageKeeper.GetAllContracts(suite.ctx)
+	aliceContract := allContracts[0]
+	charlieContract := allContracts[1]
+
+	suite.Require().NoError(err)
+	suite.Require().Equal(aliceContract, contract)
+	suite.Require().Equal(charlieContract, contract1)
+
+}
+
+func (suite *KeeperTestSuite) TestRemoveContracts() {
+	suite.SetupSuite()
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
+	suite.Require().NoError(err)
+
+	contract := types.Contracts{
+		Cid:      "549",
+		Merkle:   "",
+		Signee:   user.String(),
+		Duration: "1000",
+		Filesize: "5000",
+		Fid:      "5789",
+		Creator:  provider.String(),
+	}
+
+	suite.storageKeeper.SetContracts(suite.ctx, contract)
+	suite.Require().NoError(err)
+
+	suite.storageKeeper.RemoveContracts(suite.ctx, "549")
+
+	foundContract, found := suite.storageKeeper.GetContracts(suite.ctx, "549")
+
+	ghostContract := types.Contracts{}
+
+	suite.Require().NoError(err)
+	suite.Require().Equal(found, false)
+	suite.Require().Equal(foundContract, ghostContract)
+
+}
+
 func (suite *KeeperTestSuite) TestSetActiveDeals() {
 	suite.SetupSuite()
 	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
@@ -79,44 +222,6 @@ func (suite *KeeperTestSuite) TestGetActiveDeals() {
 
 }
 
-func (suite *KeeperTestSuite) TestRemoveActiveDeals() {
-	suite.SetupSuite()
-	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
-	suite.Require().NoError(err)
-
-	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
-	suite.Require().NoError(err)
-
-	deal := types.ActiveDeals{
-		Cid:           "549",
-		Signee:        user.String(),
-		Provider:      provider.String(),
-		Startblock:    "100",
-		Endblock:      "1000",
-		Filesize:      "5000",
-		Proofverified: "true",
-		Proofsmissed:  "0",
-		Blocktoprove:  "150",
-		Creator:       user.String(),
-		Merkle:        "",
-		Fid:           "5789",
-	}
-
-	suite.storageKeeper.SetActiveDeals(suite.ctx, deal)
-	suite.Require().NoError(err)
-
-	suite.storageKeeper.RemoveActiveDeals(suite.ctx, "549")
-
-	foundDeal, found := suite.storageKeeper.GetActiveDeals(suite.ctx, "549")
-
-	ghostDeal := types.ActiveDeals{}
-
-	suite.Require().NoError(err)
-	suite.Require().Equal(found, false)
-	suite.Require().Equal(foundDeal, ghostDeal)
-
-}
-
 func (suite *KeeperTestSuite) TestGetAllActiveDeals() {
 	suite.SetupSuite()
 
@@ -172,5 +277,43 @@ func (suite *KeeperTestSuite) TestGetAllActiveDeals() {
 	suite.Require().NoError(err)
 	suite.Require().Equal(aliceDeal, deal)
 	suite.Require().Equal(charleDeal, deal1)
+
+}
+
+func (suite *KeeperTestSuite) TestRemoveActiveDeals() {
+	suite.SetupSuite()
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	provider, err := sdk.AccAddressFromBech32("cosmos17j2hkm7n9fz9dpntyj2kxgxy5pthzd289nvlfl")
+	suite.Require().NoError(err)
+
+	deal := types.ActiveDeals{
+		Cid:           "549",
+		Signee:        user.String(),
+		Provider:      provider.String(),
+		Startblock:    "100",
+		Endblock:      "1000",
+		Filesize:      "5000",
+		Proofverified: "true",
+		Proofsmissed:  "0",
+		Blocktoprove:  "150",
+		Creator:       user.String(),
+		Merkle:        "",
+		Fid:           "5789",
+	}
+
+	suite.storageKeeper.SetActiveDeals(suite.ctx, deal)
+	suite.Require().NoError(err)
+
+	suite.storageKeeper.RemoveActiveDeals(suite.ctx, "549")
+
+	foundDeal, found := suite.storageKeeper.GetActiveDeals(suite.ctx, "549")
+
+	ghostDeal := types.ActiveDeals{}
+
+	suite.Require().NoError(err)
+	suite.Require().Equal(found, false)
+	suite.Require().Equal(foundDeal, ghostDeal)
 
 }
