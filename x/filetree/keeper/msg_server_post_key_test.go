085a45b8644bc60bf35d647d3e4e7c1f7f9f3590
BiPhan4 <biphan374@gmail.com>
2022-11-22 15:00:55 -0500

    test helpers added. TestMsgPostKey passes

---
+++ x/filetree/keeper/msg_server_post_key_test.go
@@ -0,0 +1,49 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/jackalLabs/canine-chain/x/filetree/types"
+)
+
+func (suite *KeeperTestSuite) TestMsgPostKey() {
+	suite.SetupSuite()
+
+	msgSrvr, _, context := setupMsgServer(suite)
+
+	user, err := sdk.AccAddressFromBech32("cosmos1ytwr7x4av05ek0tf8z9s4zmvr6w569zsm27dpg")
+	suite.Require().NoError(err)
+
+	cases := []struct {
+		preRun    func() *types.MsgPostkey
+		expErr    bool
+		expErrMsg string
+		name      string
+	}{
+		{
+			preRun: func() *types.MsgPostkey {
+				return types.NewMsgPostkey(
+					user.String(),
+					"public key",
+				)
+			},
+			expErr: false,
+			name:   "post key success",
+		},
+	}
+
+	for _, tc := range cases {
+		suite.Run(tc.name, func() {
+			msg := tc.preRun()
+			suite.Require().NoError(err)
+			res, err := msgSrvr.Postkey(context, msg)
+			if tc.expErr {
+				suite.Require().Error(err)
+				suite.Require().Contains(err.Error(), tc.expErrMsg)
+			} else {
+				suite.Require().NoError(err)
+				suite.Require().EqualValues(types.MsgPostkeyResponse{}, *res)
+
+			}
+		})
+	}
+}
