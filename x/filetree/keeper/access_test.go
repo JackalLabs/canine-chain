928fe163953240932b3cd29c6656a623f03ff596
Daniel Ahn <dahn510@gmail.com>
2022-11-24 16:05:06 -0800

    added fuzz testing to HasViewingAccess()

--- x/filetree/keeper/access_test.go
+++ x/filetree/keeper/access_test.go
@@ -4,10 +4,69 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"testing"
 
 	"github.com/jackalLabs/canine-chain/x/filetree/keeper"
 	"github.com/jackalLabs/canine-chain/x/filetree/types"
 )
+
+func FuzzHasEditAccess(f *testing.F) {
+	cases := []struct {
+		editAccess  string
+		trackingNum string
+		user        string
+	}{
+		{
+			editAccess:  "aaaaaaaa",
+			trackingNum: "111111111",
+			user:        "",
+		},
+
+		{
+			editAccess:  "",
+			trackingNum: "someNum",
+			user:        "someUser",
+		},
+
+		{
+			editAccess:  `"diff_addr_str": "a"`,
+			trackingNum: "someNum",
+			user:        "someUser",
+		},
+	}
+
+	for _, tc := range cases {
+		f.Add(tc.editAccess, tc.trackingNum, tc.user)
+	}
+	
+	f.Fuzz(func(t *testing.T, edit, track, user string){
+		f := types.Files{
+			EditAccess: edit,
+			TrackingNumber: track,
+		}
+
+		out, err := keeper.HasEditAccess(f, user)
+
+		if !json.Valid([]byte(edit)) {
+			// The function should return error when invalid json is passed
+			if err == nil {
+				t.Errorf("Passed invalid json: %s but didn't get error.", edit)
+			}
+
+			h := sha256.New()
+			h.Write([]byte(fmt.Sprintf("e%s%s", track, user)))
+			hash := h.Sum(nil)
+
+			jeacc := make(map[string]string)
+			_ = json.Unmarshal([]byte(edit), &jeacc)
+			_, expOut := jeacc[fmt.Sprintf("%x", hash)]
+
+			if expOut != out {
+				t.Errorf("Expected: %t, got: %t", expOut, out)
+			}
+		}
+	})
+}
 
 func (suite *KeeperTestSuite) TestHasEditAccess() {
 	cases := map[string]struct {
