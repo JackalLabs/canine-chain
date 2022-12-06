f94d9b0e11eed0bdd4d28b04c72e72accd764230
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-12-06 13:06:35 -0500

    fix for strays

--- x/storage/keeper/msg_server_cancel_contract.go
+++ x/storage/keeper/msg_server_cancel_contract.go
@@ -2,8 +2,10 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	"io"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
@@ -14,27 +16,87 @@ import (
 func (k msgServer) CancelContract(goCtx context.Context, msg *types.MsgCancelContract) (*types.MsgCancelContractResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	deal, found := k.GetActiveDeals(ctx, msg.Cid)
-	if !found {
-		return nil, fmt.Errorf("can't find contract")
-	}
+	root := msg.Cid
 
-	if deal.Creator != msg.Creator {
-		return nil, fmt.Errorf("you don't own this deal")
-	}
-
-	ftc, found := k.GetFidCid(ctx, deal.Fid)
-	if !found {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "no fid found")
-	}
-	var ncids []string
-	err := json.Unmarshal([]byte(ftc.Cids), &ncids)
+	h := sha256.New()
+	_, err := io.WriteString(h, fmt.Sprintf("%s%d", root, 0))
 	if err != nil {
 		return nil, err
 	}
+	hashName := h.Sum(nil)
+
+	left, err := MakeCid(hashName)
+	if err != nil {
+		return nil, err
+	}
+	h = sha256.New()
+	_, err = io.WriteString(h, fmt.Sprintf("%s%d", root, 1))
+	if err != nil {
+		return nil, err
+	}
+	hashName = h.Sum(nil)
+
+	right, err := MakeCid(hashName)
+	if err != nil {
+		return nil, err
+	}
+
+	var fid string
+
+	d, found := k.GetActiveDeals(ctx, root)
+	if !found {
+		s, found := k.GetStrays(ctx, root)
+		if !found {
+			return nil, fmt.Errorf("can't find contract")
+		}
+		k.RemoveStrays(ctx, s.Cid)
+	} else {
+		k.RemoveActiveDeals(ctx, d.Cid)
+	}
+
+	d, found = k.GetActiveDeals(ctx, left)
+	if !found {
+		s, found := k.GetStrays(ctx, left)
+		if !found {
+			return nil, fmt.Errorf("can't find contract")
+		}
+		k.RemoveStrays(ctx, s.Cid)
+	} else {
+		k.RemoveActiveDeals(ctx, d.Cid)
+	}
+
+	d, found = k.GetActiveDeals(ctx, right)
+	if !found {
+		s, found := k.GetStrays(ctx, right)
+		if !found {
+			return nil, fmt.Errorf("can't find contract")
+		} else {
+			fid = s.Fid
+			k.RemoveStrays(ctx, s.Cid)
+		}
+	} else {
+		fid = d.Fid
+		k.RemoveActiveDeals(ctx, d.Cid)
+	}
+
+	if d.Creator != msg.Creator {
+		return nil, fmt.Errorf("you don't own this deal")
+	}
+
+	ftc, found := k.GetFidCid(ctx, fid)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "no fid found")
+	}
+
+	var ncids []string
+	err = json.Unmarshal([]byte(ftc.Cids), &ncids)
+	if err != nil {
+		return nil, err
+	}
+
 	cids := make([]string, 0)
 	for _, v := range ncids {
-		if v != msg.Cid {
+		if v != root && v != left && v != right {
 			cids = append(cids, v)
 		}
 	}
@@ -45,8 +107,6 @@ func (k msgServer) CancelContract(goCtx context.Context, msg *types.MsgCancelCon
 	ftc.Cids = string(b)
 
 	k.SetFidCid(ctx, ftc)
-
-	k.RemoveActiveDeals(ctx, deal.Cid)
 
 	return &types.MsgCancelContractResponse{}, nil
 }
