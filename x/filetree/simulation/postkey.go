69fefd4eb9f1c113271d54caa228f41a2aac4c15
Nugget <nuggetnchill@gmail.com>
2023-02-04 13:54:36 +0700

    postkey

--- x/filetree/simulation/postkey.go
+++ x/filetree/simulation/postkey.go
@@ -1,11 +1,14 @@
 package simulation
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
 	"github.com/jackalLabs/canine-chain/x/filetree/keeper"
 	"github.com/jackalLabs/canine-chain/x/filetree/types"
 )
@@ -17,13 +20,54 @@ func SimulateMsgPostkey(
 ) simtypes.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
 	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		// merklePath := types.MerklePath("s/home/")
 		simAccount, _ := simtypes.RandomAcc(r, accs)
+		address, _ := sdk.Bech32ifyAddressBytes("jkl", simAccount.Address)
+		// accountHash := types.HashThenHex(address)
+		//
+		// // root folder
+		// rootFolder, err := types.CreateRootFolder(address)
+		// if err != nil {
+		// 	return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgPostkey, "unable to create root folder"), nil, err
+		// }
+		// k.SetFiles(ctx, *rootFolder)
+		//
+		// // home folder
+		// homeFolder, err := types.CreateFolderOrFile(address, strings.Split(address, ","), strings.Split(address, ","), "s/home/")
+		// if err != nil {
+		// 	return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgPostkey, "unable to create home folder"), nil, err
+		// }
+		// k.SetFiles(ctx, *homeFolder)
+		privateKey, err := types.MakePrivateKey(address)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgPostkey, "unable to create privateKey"), nil, err
+		}
+		pubKey := privateKey.PublicKey.Bytes(false)
+
 		msg := &types.MsgPostkey{
 			Creator: simAccount.Address.String(),
+			Key:     fmt.Sprintf("%x", pubKey),
 		}
 
-		// TODO: Handling the Postkey simulation
+		spendable := bk.SpendableCoins(ctx, simAccount.Address)
+		fees, err := simtypes.RandomFees(r, ctx, spendable)
+		if err != nil {
+			return simtypes.NoOpMsg(types.ModuleName, types.TypeMsgPostkey, "failed to generate fee"), nil, err
+		}
 
-		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "Postkey simulation not implemented"), nil, nil
+		txCtx := simulation.OperationInput{
+			R:             r,
+			App:           app,
+			TxGen:         simappparams.MakeTestEncodingConfig().TxConfig,
+			Cdc:           nil,
+			Msg:           msg,
+			MsgType:       msg.Type(),
+			Context:       ctx,
+			SimAccount:    simAccount,
+			AccountKeeper: ak,
+			ModuleName:    types.ModuleName,
+		}
+
+		return simulation.GenAndDeliverTx(txCtx, fees)
 	}
 }
