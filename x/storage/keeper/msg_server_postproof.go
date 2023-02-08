2dd5386f534ea99cf6c3ce18febc70ec333ae91a
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-02-08 11:24:34 -0500

    error message

--- x/storage/keeper/msg_server_postproof.go
+++ x/storage/keeper/msg_server_postproof.go
@@ -25,25 +25,25 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	if !found {
 		ctx.Logger().Debug("%s, %s\n", "Contract not found", msg.Cid)
 
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: fmt.Sprintf("contract not found: %s", msg.Cid)}, nil
 	}
 
 	if contract.Proofverified == "true" {
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "proof already verified"}, nil
 	}
 
 	ctx.Logger().Debug("Contract that was found: \n%v\n", contract)
 
 	nn, ok := sdk.NewIntFromString(contract.Blocktoprove)
 	if !ok {
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "cannot parse block to prove"}, nil
 	}
 	num := nn.Int64()
 
 	h := sha256.New()
 	_, err := io.WriteString(h, fmt.Sprintf("%d%s", num, msg.Item))
 	if err != nil {
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 	hashName := h.Sum(nil)
 
@@ -54,7 +54,7 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	err = json.Unmarshal([]byte(msg.Hashlist), &proof)
 	if err != nil {
 		ctx.Logger().Debug("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 
 	ctx.Logger().Debug("proof: %v\n", proof)
@@ -62,18 +62,18 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	m, err := hex.DecodeString(contract.Merkle)
 	if err != nil {
 		ctx.Logger().Error("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 	verified, err := merkletree.VerifyProofUsing(hashName, false, &proof, [][]byte{m}, sha3.New512())
 	if err != nil {
 		ctx.Logger().Error("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 
 	if !verified {
 		ctx.Logger().Debug("%s\n", "Cannot verify")
 
-		return &types.MsgPostproofResponse{Success: false}, nil
+		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "cannot verify proof"}, nil
 	}
 
 	ctx.GasMeter().RefundGas(ctx.GasMeter().GasConsumed(), "successful proof refund.")
@@ -81,5 +81,5 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	contract.Proofverified = "true"
 	k.SetActiveDeals(ctx, contract)
 
-	return &types.MsgPostproofResponse{Success: true}, nil
+	return &types.MsgPostproofResponse{Success: true, ErrorMessage: ""}, nil
 }
