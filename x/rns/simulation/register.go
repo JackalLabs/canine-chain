0a4193857c268fa47d16e3d4a8420cacdc31e7ff
Daniel Ahn <dahn510@gmail.com>
2023-02-10 10:16:45 -0800

    fix register

--- x/rns/simulation/register.go
+++ x/rns/simulation/register.go
@@ -24,18 +24,10 @@ func SimulateMsgRegister(
 		msg := &types.MsgRegister{
 			Creator: simAccount.Address.String(),
 		}
-		// generating a random name
-		// generating a random TLD
-		tldIndex := simtypes.RandIntBetween(r, 0, len(types.SupportedTLDs))
-		tld := types.SupportedTLDs[tldIndex]
-		// generating a random name
-		nameLength := simtypes.RandIntBetween(r, 1, 10)
-		name := StringWithCharset(r, nameLength, charset)
-		// fullDomain := name + "." + tld // for debugging purposes
-		// fmt.Print(fullDomain)
-		// generating a random time in years
+		tld := types.SupportedTLDs[r.Intn(len(types.SupportedTLDs))]
+		name := simtypes.RandStringOfLength(r, simtypes.RandIntBetween(r, 1, 15))
 		numYears := simtypes.RandIntBetween(r, 1, 15)
-		blockHeight := ctx.BlockTime()
+		blockHeight := ctx.BlockHeight()
 		time := int64(numYears) * 5733818
 
 		// checking if the domain already exists on-chain
@@ -43,23 +35,21 @@ func SimulateMsgRegister(
 		if isFound {
 			if whois.Value == msg.Creator {
 				time = whois.Expires + time
-			} else if blockHeight.Unix() < whois.Expires {
+			} else if blockHeight < whois.Expires {
 				return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "Name is already registered"), nil, nil
 			}
 		} else {
-			time += blockHeight.Unix()
+			time += blockHeight
 		}
 
 		// calculating the necessary costs to rent the domain
 		domainCost, err := keeper.GetCostOfName(name, tld)
 		if err != nil {
-			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "Could not grab the cost of name"), nil, nil
+			return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "Could not grab the cost of name"), nil, err
 		}
-		var cost sdk.Int
-		cost = sdk.NewInt(domainCost)
 
 		var price sdk.Int
-		price = sdk.NewInt(cost.Int64() * int64(numYears))
+		price = sdk.NewInt(domainCost * int64(numYears))
 
 		// ensuring the account has enough coins to buy the domain
 		jBalance := bk.GetBalance(ctx, simAccount.Address, "ujkl")
