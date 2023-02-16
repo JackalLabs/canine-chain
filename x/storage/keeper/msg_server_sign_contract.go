62002b844e167a17750576d120228ed7adc814d1
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-02-15 20:09:51 -0500

    Revert "Fixed according to erin's suggestions"
    This reverts commit 3143c7b947fa9050f0b96b11797277f0eada3af2.

--- x/storage/keeper/msg_server_sign_contract.go
+++ x/storage/keeper/msg_server_sign_contract.go
@@ -8,7 +8,6 @@ import (
 	"io"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/jackalLabs/canine-chain/x/storage/types"
 )
 
@@ -87,18 +86,17 @@ func (k msgServer) SignContract(goCtx context.Context, msg *types.MsgSignContrac
 			return nil, fmt.Errorf("cannot parse file size")
 		}
 		payInfo, found := k.GetStoragePaymentInfo(ctx, msg.Creator)
-		if !found {
-			return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "payment info not found, please purchase storage space")
+		if found {
+			// check if user has any free space
+			if payInfo.SpaceUsed+(fsize.Int64()*3) > payInfo.SpaceAvailable {
+				return nil, fmt.Errorf("not enough storage space")
+			}
+			// check if storage subscription still active
+			if payInfo.End.Before(ctx.BlockTime()) {
+				return nil, fmt.Errorf("storage subscription has expired")
+			}
 		}
-
-		// check if user has any free space
-		if payInfo.SpaceUsed+(fsize.Int64()*3) > payInfo.SpaceAvailable {
-			return nil, fmt.Errorf("not enough storage space")
-		}
-		// check if storage subscription still active
-		if payInfo.End.Before(ctx.BlockTime()) {
-			return nil, fmt.Errorf("storage subscription has expired")
-		}
+		// we going to need an else statement to check for the free trial storage since they wont have payInfo
 
 		payInfo.SpaceUsed += fsize.Int64() * 3
 
