e8c90d9ed7571675390269c514f469231a010ccb
Nugget <nuggetnchill@gmail.com>
2023-03-01 14:20:58 +0700

    updated rns simulation

---
+++ x/rns/simulation/param.go
@@ -0,0 +1,20 @@
+package simulation
+
+import (
+	"fmt"
+	"math/rand"
+
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/jackalLabs/canine-chain/x/rns/types"
+)
+
+func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
+	return []simtypes.ParamChange{
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyDepositAccount),
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", "jkl14c3j672kvw9l5uleh4x9uds2fre5vl7yun4ss8")
+			},
+		),
+	}
+}
