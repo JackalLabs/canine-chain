5777587b0721bba204202a8005724f7ade2dd324
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-03-11 11:38:20 -0500

    rearranging code for debugging

--- x/rns/keeper/grpc_query_params.go
+++ x/rns/keeper/grpc_query_params.go
@@ -15,5 +15,6 @@ func (k Keeper) Params(c context.Context, req *types.QueryParamsRequest) (*types
 	}
 	ctx := sdk.UnwrapSDKContext(c)
 
-	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+	p := k.GetParams(ctx)
+	return &types.QueryParamsResponse{Params: p}, nil
 }
