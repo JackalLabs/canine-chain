1383a9759d0dee67afc635d54ae5c50f84ef89ce
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-03-12 11:52:19 -0400

    creating upgrade script for mainnet

---
+++ scripts/upgrade-test-mainnet.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+UPGRADE_HEIGHT=26
+HOME=mytestnet
+ROOT=$(pwd)
+DENOM=ujkl
+SOFTWARE_UPGRADE_NAME=$3
+
+# underscore so that go tool will not take gocache into account
+mkdir -p ${ROOT}/../_build/gocache
+export GOMODCACHE=${ROOT}/../_build/gocache
+
+# install old binary
+
+mkdir -p ../_build/old
+git checkout $OLD_VERSION
+make build
+mv build/canined ./../_build/old/canined
+git checkout $NEW_VERSION
+make install
+
+# start old node
+screen -dmS node1 bash scripts/run-upgrade-node.sh ./../_build/old/canined $DENOM
+
+./../_build/old/canined version --home $HOME
+
+sleep 20
+
+./../_build/old/canined tx gov submit-proposal software-upgrade "$SOFTWARE_UPGRADE_NAME" --upgrade-height $UPGRADE_HEIGHT --upgrade-info "temp" --title "upgrade" --description "upgrade"  --from test1 --keyring-backend test --chain-id test --home $HOME -y
+
+sleep 6
+
+./../_build/old/canined tx gov deposit 1 "20000000${DENOM}" --from test1 --keyring-backend test --chain-id test --home $HOME -y
+
+sleep 6
+
+./../_build/old/canined tx gov vote 1 yes --from test --keyring-backend test --chain-id test --home $HOME -y
+
+sleep 6
+
+./../_build/old/canined tx gov vote 1 yes --from test1 --keyring-backend test --chain-id test --home $HOME -y
+
+sleep 6
+
+# determine block_height to halt
+while true; do 
+    BLOCK_HEIGHT=$(./../_build/old/canined status | jq '.SyncInfo.latest_block_height' -r)
+    if [ $BLOCK_HEIGHT = "$UPGRADE_HEIGHT" ]; then
+        # assuming running only 1 canined
+        echo "BLOCK HEIGHT = $UPGRADE_HEIGHT REACHED, KILLING OLD ONE"
+        pkill canined
+        break
+    else
+        ./../_build/old/canined q storage list-active-deals --chain-id test --home $HOME
+        ./../_build/old/canined q storage list-strays --chain-id test --home $HOME
+        ./../_build/old/canined q storage list-contracts --chain-id test --home $HOME
+        ./../_build/old/canined q gov proposal 1 --output=json | jq ".status"
+        echo "BLOCK_HEIGHT = $BLOCK_HEIGHT"
+        sleep 2
+    fi
+done
+
+sleep 3
+canined start --home $HOME
+
