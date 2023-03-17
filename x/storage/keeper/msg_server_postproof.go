d590c2d4ebeb5ce6b96e0ce69573cdb7d8cc98af
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-03-17 09:29:48 -0400

    refund gas correctly

--- x/storage/keeper/msg_server_postproof.go
+++ x/storage/keeper/msg_server_postproof.go
@@ -18,20 +18,25 @@ import (
 
 func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*types.MsgPostproofResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	meter := ctx.BlockGasMeter()
+	usedGas := meter.GasConsumed()
+	var refunded uint64
 
 	hashes := strings.Split(msg.Hashlist, ",")
 
 	contract, found := k.GetActiveDeals(ctx, msg.Cid)
 	if !found {
 		ctx.Logger().Debug("%s, %s\n", "Contract not found", msg.Cid)
-
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: fmt.Sprintf("contract not found: %s", msg.Cid)}, nil
 	}
+	refunded += meter.GasConsumed() - usedGas
+	meter.RefundGas(meter.GasConsumed()-usedGas, "gas limiting")
 
 	ctx.Logger().Debug("Contract that was found: \n%v\n", contract)
 
 	nn, ok := sdk.NewIntFromString(contract.Blocktoprove)
 	if !ok {
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "cannot parse block to prove"}, nil
 	}
 	num := nn.Int64()
@@ -39,6 +44,7 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	h := sha256.New()
 	_, err := io.WriteString(h, fmt.Sprintf("%d%s", num, msg.Item))
 	if err != nil {
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 	hashName := h.Sum(nil)
@@ -50,6 +56,7 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	err = json.Unmarshal([]byte(msg.Hashlist), &proof)
 	if err != nil {
 		ctx.Logger().Debug("%v\n", err)
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 
@@ -58,29 +65,31 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	m, err := hex.DecodeString(contract.Merkle)
 	if err != nil {
 		ctx.Logger().Error("%v\n", err)
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 	verified, err := merkletree.VerifyProofUsing(hashName, false, &proof, [][]byte{m}, sha3.New512())
 	if err != nil {
 		ctx.Logger().Error("%v\n", err)
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
 	}
 
 	if !verified {
 		ctx.Logger().Debug("%s\n", "Cannot verify")
-
+		meter.ConsumeGas(refunded, "undoing refund")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "cannot verify proof"}, nil
 	}
 
 	if contract.Proofverified == "true" {
-		ctx.GasMeter().RefundGas(ctx.GasMeter().GasConsumed(), "already verified refund.")
 		return &types.MsgPostproofResponse{Success: false, ErrorMessage: "proof already verified"}, nil
 	}
 
 	contract.Proofverified = "true"
 	k.SetActiveDeals(ctx, contract)
 
-	ctx.GasMeter().RefundGas(ctx.GasMeter().GasConsumed(), "successful proof refund.")
+	refunded += meter.GasConsumed() - usedGas
+	meter.RefundGas(meter.GasConsumed()-usedGas, "successful proof refund")
 
 	return &types.MsgPostproofResponse{Success: true, ErrorMessage: ""}, nil
 }
