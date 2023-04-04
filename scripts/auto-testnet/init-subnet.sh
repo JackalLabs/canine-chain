ef65de9efb26a61e4760c68166f1524f182b4cff
Maximilian Pochapski <67759684+mxpoch@users.noreply.github.com>
2023-04-04 14:51:06 -0400

    added variable # of validators

--- scripts/auto-testnet/init-subnet.sh
+++ scripts/auto-testnet/init-subnet.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-set -x
+set -e
+
+# collecting keywords
+# required keywords: NUM_VALIDATORS, RELEASE, CHAIN_ID,  
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
 
 # building the validator docker image
 docker buildx build . -t validator \
 -f ValidatorDockerfile \
---build-arg RELEASE=v1.2.1 \
+--build-arg RELEASE=$RELEASE \
 --build-arg VALIDATOR_SCRIPT=genesis-validator.sh \
---build-arg CHAIN_ID=testing \
---build-arg VALIDATOR_NAME=genesis-val \
+--build-arg CHAIN_ID=$CHAIN_ID
 
 # running the docker image
 sudo docker run -d -v "/root/common_store:/home/common_store" \
@@ -17,10 +28,10 @@ validator
 # executing internal scripts
 sudo docker exec genesis-validator bash /home/canine-validator/scripts/genesis-validator.sh \
 VALIDATOR_NAME=genesis-val \
-CHAIN_ID=testing
+CHAIN_ID=$CHAIN_ID
 
 # spawning additional docker images, w/o the genesis validator
-for i in {1..3}
+for ((i=1; i <= $NUM_VALIDATORS-1; i++));
 do
     # running the docker image
     docker run -d -v "/root/common_store:/home/common_store" \
@@ -30,7 +41,7 @@ do
     # executing internal scripts
     docker exec validator_${i} bash /home/canine-validator/scripts/validator.sh \
     VALIDATOR_NAME=val_${i} \
-    CHAIN_ID=testing \
+    CHAIN_ID=$CHAIN_ID \
 
 done
 
@@ -38,8 +49,8 @@ done
 # docker exec genesis-validator rm -rf /root/.canine/config/gentx
 docker exec genesis-validator cp /home/common_store/genesis.json /root/.canine/config/genesis.json
 docker exec genesis-validator cp -RT /home/common_store/gentx /root/.canine/config/gentx
-docker exec genesis-validator canined collect-gentxs --chain-id=testing
-docker exec genesis-validator canined tendermint unsafe-reset-all --chain-id=testing 
+docker exec genesis-validator canined collect-gentxs --chain-id=$CHAIN_ID
+docker exec genesis-validator canined tendermint unsafe-reset-all --chain-id=$CHAIN_ID 
 
 # changing the genesis-validator listening address in config.toml
 export IPADDR=$(docker exec genesis-validator hostname -I | tr -d ' ')
@@ -47,14 +58,14 @@ docker exec genesis-validator sed -i "s|tcp://0\.0\.0\.0:26656|tcp://$IPADDR:266
 docker exec genesis-validator sed -i "s|tcp://127\.0\.0\.1:26657|tcp://$IPADDR:26657|" $HOME/.canine/config/config.toml
 docker exec genesis-validator sed -i "s|tcp://127\.0\.0\.1:26658|tcp://$IPADDR:26658|" $HOME/.canine/config/config.toml
 
-for i in {1..3}
+for ((i=1; i <= $NUM_VALIDATORS-1; i++));
 do
     # collecting the gentxs
     # docker exec validator_${i} rm -rf /root/.canine/config/gentx
     docker exec validator_${i} cp /home/common_store/genesis.json /root/.canine/config/genesis.json
     docker exec validator_${i} cp -RT /home/common_store/gentx /root/.canine/config/gentx
-    docker exec validator_${i} canined collect-gentxs --chain-id=testing
-    docker exec validator_${i} canined tendermint unsafe-reset-all --chain-id=testing 
+    docker exec validator_${i} canined collect-gentxs --chain-id=$CHAIN_ID
+    docker exec validator_${i} canined tendermint unsafe-reset-all --chain-id=$CHAIN_ID 
 
     # changing the genesis-validator listening address in config.toml
     export IPADDR=$(docker exec validator_${i} hostname -I | tr -d ' ')
@@ -65,8 +76,7 @@ done
 
 # starting the chain 
 docker exec -d genesis-validator canined start
-
-for i in {1..3}
+for ((i=1; i <= $NUM_VALIDATORS-1; i++));
 do
     docker exec -d validator_${i} canined start 
 done
