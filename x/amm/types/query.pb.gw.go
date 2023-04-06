3b3f3cf9553a924243e66c775984583a9cbfeb84
Daniel Ahn <dahn510@gmail.com>
2023-04-05 21:33:57 -0700

    rename query

--- x/amm/types/query.pb.gw.go
+++ x/amm/types/query.pb.gw.go
@@ -110,7 +110,7 @@ var (
 )
 
 func request_Query_PoolAll_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryAlPoolRequest
+	var protoReq QueryAllPoolRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
@@ -126,7 +126,7 @@ func request_Query_PoolAll_0(ctx context.Context, marshaler runtime.Marshaler, c
 }
 
 func local_request_Query_PoolAll_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryAlPoolRequest
+	var protoReq QueryAllPoolRequest
 	var metadata runtime.ServerMetadata
 
 	if err := req.ParseForm(); err != nil {
@@ -369,8 +369,8 @@ func local_request_Query_EstimateSwapIn_0(ctx context.Context, marshaler runtime
 
 }
 
-func request_Query_EstimateContribution_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryEstimateContributionRequest
+func request_Query_EstimatePoolJoin_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryEstimatePoolJoinRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -402,13 +402,13 @@ func request_Query_EstimateContribution_0(ctx context.Context, marshaler runtime
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "desiredAmount", err)
 	}
 
-	msg, err := client.EstimateContribution(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.EstimatePoolJoin(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Query_EstimateContribution_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryEstimateContributionRequest
+func local_request_Query_EstimatePoolJoin_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryEstimatePoolJoinRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -440,7 +440,7 @@ func local_request_Query_EstimateContribution_0(ctx context.Context, marshaler r
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "desiredAmount", err)
 	}
 
-	msg, err := server.EstimateContribution(ctx, &protoReq)
+	msg, err := server.EstimatePoolJoin(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -791,7 +791,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
-	mux.Handle("GET", pattern_Query_EstimateContribution_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_EstimatePoolJoin_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -802,7 +802,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Query_EstimateContribution_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Query_EstimatePoolJoin_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -810,7 +810,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 
-		forward_Query_EstimateContribution_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_EstimatePoolJoin_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1044,7 +1044,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Query_EstimateContribution_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_EstimatePoolJoin_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -1053,14 +1053,14 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Query_EstimateContribution_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Query_EstimatePoolJoin_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Query_EstimateContribution_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_EstimatePoolJoin_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1140,7 +1140,7 @@ var (
 
 	pattern_Query_EstimateSwapIn_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"jackal-dao", "canine-chain", "amm", "estimate_in", "poolName", "outputCoins"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_EstimateContribution_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"jackal-dao", "canine-chain", "amm", "estimate_contribution", "poolName", "desiredAmount"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_EstimatePoolJoin_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"jackal-dao", "canine-chain", "amm", "estimate_contribution", "poolName", "desiredAmount"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_MakeValidPair_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"jackal-dao", "canine-chain", "amm", "make_pair", "poolName", "coin"}, "", runtime.AssumeColonVerbOpt(false)))
 
@@ -1162,7 +1162,7 @@ var (
 
 	forward_Query_EstimateSwapIn_0 = runtime.ForwardResponseMessage
 
-	forward_Query_EstimateContribution_0 = runtime.ForwardResponseMessage
+	forward_Query_EstimatePoolJoin_0 = runtime.ForwardResponseMessage
 
 	forward_Query_MakeValidPair_0 = runtime.ForwardResponseMessage
 
