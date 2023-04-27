389c619f9927e944d108d245d17a9fbd06347f4e
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-04-26 23:24:11 -0400

    api update

--- x/filetree/keeper/msg_server_make_root.go
+++ x/filetree/keeper/msg_server_make_root.go
@@ -2,31 +2,48 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/jackalLabs/canine-chain/x/filetree/types"
 )
 
-func (k msgServer) MakeRoot(goCtx context.Context, msg *types.MsgMakeRoot) (*types.MsgMakeRootResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
+func (k msgServer) MakeRootFolder(ctx sdk.Context, creator string, viewers string, editors string, trackingNumber string) {
 	merklePath := types.MerklePath("s")
 
-	// msg.Account was already hex(hashed) before it go to here.
-	// make the full OwnerAddress
+	h1 := sha256.New() // making full address
+	h1.Write([]byte(creator))
+	hash1 := h1.Sum(nil)
 
-	ownerAddress := MakeOwnerAddress(msg.RootHashPath, msg.Account)
+	accountHash := fmt.Sprintf("%x", hash1)
+
+	ownerAddress := MakeOwnerAddress(merklePath, accountHash)
 
 	file := types.Files{
-		Contents:       msg.Contents,
+		Contents:       "",
 		Owner:          ownerAddress,
-		ViewingAccess:  msg.Viewers,
-		EditAccess:     msg.Editors,
+		ViewingAccess:  viewers,
+		EditAccess:     editors,
 		Address:        merklePath,
-		TrackingNumber: msg.TrackingNumber,
+		TrackingNumber: trackingNumber,
 	}
 
 	k.SetFiles(ctx, file)
+}
+
+func (k msgServer) MakeRoot(goCtx context.Context, msg *types.MsgMakeRoot) (*types.MsgMakeRootResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	k.MakeRootFolder(ctx, msg.Creator, msg.Viewers, msg.Editors, msg.TrackingNumber)
 
 	return &types.MsgMakeRootResponse{}, nil
+}
+
+func (k msgServer) MakeRootV2(goCtx context.Context, msg *types.MsgMakeRootV2) (*types.MsgMakeRootV2Response, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	k.MakeRootFolder(ctx, msg.Creator, msg.Viewers, msg.Editors, msg.TrackingNumber)
+
+	return &types.MsgMakeRootV2Response{}, nil
 }
