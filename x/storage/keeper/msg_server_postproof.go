330081392144747a8ddee5535fd61435e30c512d
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-05-19 11:46:56 -0400

    separate verification from the message

--- x/storage/keeper/msg_server_postproof.go
+++ x/storage/keeper/msg_server_postproof.go
@@ -16,6 +16,32 @@ import (
 	"github.com/jackalLabs/canine-chain/x/storage/types"
 )
 
+func VerifyDeal(merkle string, hashList string, num int64, item string) bool {
+	h := sha256.New()
+	_, err := io.WriteString(h, fmt.Sprintf("%d%s", num, item))
+	if err != nil {
+		return false
+	}
+	hashName := h.Sum(nil)
+
+	var proof merkletree.Proof // unmarshal proof
+	err = json.Unmarshal([]byte(hashList), &proof)
+	if err != nil {
+		return false
+	}
+
+	m, err := hex.DecodeString(merkle)
+	if err != nil {
+		return false
+	}
+	verified, err := merkletree.VerifyProofUsing(hashName, false, &proof, [][]byte{m}, sha3.New512())
+	if err != nil {
+		return false
+	}
+
+	return verified
+}
+
 func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*types.MsgPostproofResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	meter := ctx.GasMeter()
@@ -37,35 +63,9 @@ func (k msgServer) Postproof(goCtx context.Context, msg *types.MsgPostproof) (*t
 	}
 	num := nn.Int64()
 
-	h := sha256.New()
-	_, err := io.WriteString(h, fmt.Sprintf("%d%s", num, msg.Item))
-	if err != nil {
-		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
-	}
-	hashName := h.Sum(nil)
-
 	ctx.Logger().Debug("%v\n", hashes)
 
-	var proof merkletree.Proof
-
-	err = json.Unmarshal([]byte(msg.Hashlist), &proof)
-	if err != nil {
-		ctx.Logger().Debug("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
-	}
-
-	ctx.Logger().Debug("proof: %v\n", proof)
-
-	m, err := hex.DecodeString(contract.Merkle)
-	if err != nil {
-		ctx.Logger().Error("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
-	}
-	verified, err := merkletree.VerifyProofUsing(hashName, false, &proof, [][]byte{m}, sha3.New512())
-	if err != nil {
-		ctx.Logger().Error("%v\n", err)
-		return &types.MsgPostproofResponse{Success: false, ErrorMessage: err.Error()}, nil
-	}
+	verified := VerifyDeal(contract.Merkle, msg.Hashlist, num, msg.Item)
 
 	if !verified {
 		ctx.Logger().Debug("%s\n", "Cannot verify")
