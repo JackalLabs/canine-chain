b868952d1866c331b2e22f861992b05f16fa915e
BiPhan4 <biphan374@gmail.com>
2023-05-23 15:36:17 +1200

    fixed CustomQuerier

--- wasmbinding/queries.go
+++ wasmbinding/queries.go
@@ -1,6 +1,10 @@
 package wasmbinding
 
 import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/jackalLabs/canine-chain/wasmbinding/bindings"
 	filetreekeeper "github.com/jackalLabs/canine-chain/x/filetree/keeper"
 )
 
@@ -15,5 +19,13 @@ func NewQueryPlugin(ftk *filetreekeeper.Keeper) *QueryPlugin {
 	}
 }
 
-// !!! MUST DO !!!
-// Implement a query
+// GetFiles is a query to get a Files struct
+// address is the full merklePath() of the file
+func (qp QueryPlugin) GetFiles(ctx sdk.Context, address string, ownerAddress string) (*bindings.FilesResponse, error) {
+	file, found := qp.filetreeKeeper.GetFiles(ctx, address, ownerAddress)
+	if !found {
+		return nil, fmt.Errorf("failed to get file: %s", file) // file is blank or empty struct if not found?
+	}
+
+	return &bindings.FilesResponse{Files: file.String()}, nil
+}
--- wasmbinding/query_plugin.go
+++ wasmbinding/query_plugin.go
@@ -1,17 +1,13 @@
 package wasmbinding
 
 import (
-	"context"
 	"encoding/json"
-
-	grpc1 "github.com/gogo/protobuf/grpc"
 
 	errorsmod "cosmossdk.io/errors"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	bindings "github.com/jackalLabs/canine-chain/wasmbinding/bindings"
-	filetreetypes "github.com/jackalLabs/canine-chain/x/filetree/types"
 )
 
 // CustomQuerier dispatches custom CosmWasm bindings queries.
@@ -24,38 +20,18 @@ func CustomQuerier(qp *QueryPlugin) func(ctx sdk.Context, request json.RawMessag
 
 		switch {
 		case contractQuery.Files != nil:
-			owner := contractQuery.Files.Owner
 			address := contractQuery.Files.Address
+			owner := contractQuery.Files.Owner
 
-			// Osmosis uses GetFullDenom which calls tokenfactorytypes.GetTokenDenom(contract, subDenom)
-			// which simply just builds a string. This is not useful to us.
-			// We can fork wasmd to expose the keeper, or expose the keeper some other way, but this
-			// is a huge security vulnerability.
-			// Perhaps we can just use the query client here? If this package is not a full-on module, does it slow the chain down at all?
+			// As it turns out, it's possible to safely use the keeper's GetFiles() method to return the Files struct
+			// Using the query client is always still an option
 
-			// Don't need the below code any more
-			// Files, err := RetrieveFiles(ctx, filetree, owner, address)
+			res, err := qp.GetFiles(ctx, address, owner)
 
-			var cc grpc1.ClientConn
-
-			queryClient := filetreetypes.NewQueryClient(cc)
-
-			params := &filetreetypes.QueryFileRequest{
-				Address:      address,
-				OwnerAddress: owner,
-			}
-
-			queryRes, err := queryClient.Files(context.Background(), params)
-			if err != nil {
-				return nil, errorsmod.Wrap(err, "Can't find file")
-			}
-
-			// Not 100% sure it's going to turn out right on the first go
-			resForWasmBinding := bindings.FilesResponse{
-				Files: queryRes.Files.String(),
-			}
-
-			bz, err := json.Marshal(resForWasmBinding)
+			// !! WARNING !!
+			// res is the Files struct converted to a one line string with the .String() method from files.pb.go
+			// not sure if it's possible to json.Marshal this without an intermediate step
+			bz, err := json.Marshal(res)
 			if err != nil {
 				return nil, errorsmod.Wrap(err, "Jackal Files query response")
 			}
