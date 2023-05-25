b135af71ffc1b81b5456a8c4cb8b88488723cb81
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-05-25 15:30:01 -0400

    starting upgrade handler for v4

---
+++ app/upgrades/v4/upgrades.go
@@ -0,0 +1,64 @@
+package v4
+
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v4/modules/apps/29-fee/types"
+	intertxtypes "github.com/cosmos/interchain-accounts/x/inter-tx/types"
+	"github.com/jackalLabs/canine-chain/app/upgrades"
+	storagekeeper "github.com/jackalLabs/canine-chain/x/storage/keeper"
+	storagemoduletypes "github.com/jackalLabs/canine-chain/x/storage/types"
+)
+
+var _ upgrades.Upgrade = &Upgrade{}
+
+// Upgrade represents the v4 upgrade
+type Upgrade struct {
+	mm           *module.Manager
+	configurator module.Configurator
+	sk           storagekeeper.Keeper
+}
+
+// NewUpgrade returns a new Upgrade instance
+func NewUpgrade(mm *module.Manager, configurator module.Configurator, sk storagekeeper.Keeper) *Upgrade {
+	return &Upgrade{
+		mm:           mm,
+		configurator: configurator,
+		sk:           sk,
+	}
+}
+
+// Name implements upgrades.Upgrade
+func (u *Upgrade) Name() string {
+	return "v4"
+}
+
+// Handler implements upgrades.Upgrade
+func (u *Upgrade) Handler() upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		fromVM[storagemoduletypes.ModuleName] = 4
+
+		newVM, err := u.mm.RunMigrations(ctx, u.configurator, fromVM)
+		if err != nil {
+			return newVM, err
+		}
+
+		return newVM, err
+	}
+}
+
+// StoreUpgrades implements upgrades.Upgrade
+func (u *Upgrade) StoreUpgrades() *storetypes.StoreUpgrades {
+	return &storetypes.StoreUpgrades{
+		Added: []string{
+			ibcfeetypes.StoreKey,
+		},
+		Deleted: []string{
+			intertxtypes.StoreKey, // removing ICA host and controller modules for IBC-Go v4
+			"icahost",
+			"icacontroller",
+		},
+	}
+}
---
+++ x/storage/legacy/v4/store.go
@@ -0,0 +1,16 @@
+package v4
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+)
+
+// MigrateStore performs in-place store migrations from v1 to v2
+// The things done here are the following:
+// 1. setting up the next reason id and report id keys for existing subspaces
+// 2. setting up the module params
+func MigrateStore(ctx sdk.Context, paramsSubspace *paramstypes.Subspace) error {
+	ctx.Logger().Error("MIGRATING STORAGE STORE!")
+
+	return nil
+}
