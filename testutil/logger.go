18892619c259e3c5c50852f128d1c9c02477ade7
BiPhan4 <biphan374@gmail.com>
2023-05-30 23:38:37 +1200

    Use old logger. New one not working

--- testutil/logger.go
+++ testutil/logger.go
@@ -3,13 +3,10 @@ package testutil
 import (
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func CreateLogger() (log.Logger, *os.File) {
-	dir := "~/jackal/canine-chain/testutil"
-	logFilePath := filepath.Join(dir, "logs.log")
-	f, _ := os.OpenFile(logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o666)
+	f, _ := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0o666)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 	log.SetOutput(f)
 
