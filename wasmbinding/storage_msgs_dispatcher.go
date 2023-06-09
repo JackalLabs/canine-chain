96da8133e70388f8489a211263d1fe9a110985bb
BiPhan4 <biphan374@gmail.com>
2023-06-09 22:12:37 +1200

    can only use wasmd default dispatcher!

--- wasmbinding/storage_msgs_dispatcher.go
+++ wasmbinding/storage_msgs_dispatcher.go
@@ -1,8 +1,6 @@
 package wasmbinding
 
 import (
-	"encoding/json"
-
 	errorsmod "cosmossdk.io/errors"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,28 +8,6 @@ import (
 	storagekeeper "github.com/jackalLabs/canine-chain/x/storage/keeper"
 	storagetypes "github.com/jackalLabs/canine-chain/x/storage/types"
 )
-
-// STUB
-// Remember to add in the sender
-// Another dispatcher function that can be used to organise dispatching
-// the storage module's messages in a different file
-
-func (m *CustomMessenger) DispatchStorageMsg(ctx sdk.Context, contractAddr sdk.AccAddress, contractIBCPortID string, msg wasmvmtypes.CosmosMsg, sender string) ([]sdk.Event, [][]byte, error) {
-	if msg.Custom != nil {
-		// only handle the happy path where this is really posting files
-		// leave everything else for the wrapped version
-		var contractMsg bindings.JackalMsg
-		if err := json.Unmarshal(msg.Custom, &contractMsg); err != nil {
-			return nil, nil, errorsmod.Wrap(err, "Jackal msg")
-		}
-
-		if contractMsg.BuyStorage != nil {
-			return m.buyStorage(ctx, contractAddr, contractMsg.BuyStorage, sender)
-		}
-
-	}
-	return m.wrapped.DispatchMsg(ctx, contractAddr, contractIBCPortID, msg, sender)
-}
 
 func (m *CustomMessenger) buyStorage(ctx sdk.Context, contractAddr sdk.AccAddress, buyStorage *bindings.BuyStorage, sender string) ([]sdk.Event, [][]byte, error) {
 	err := PerformBuyStorage(m.storage, ctx, contractAddr, buyStorage, sender)
@@ -41,6 +17,7 @@ func (m *CustomMessenger) buyStorage(ctx sdk.Context, contractAddr sdk.AccAddres
 	return nil, nil, nil
 }
 
+// Do we care that this function is public? everything is gated by our fork of wasmd anyways
 func PerformBuyStorage(s *storagekeeper.Keeper, ctx sdk.Context, contractAddr sdk.AccAddress, buyStorage *bindings.BuyStorage, sender string) error {
 	if buyStorage == nil {
 		return wasmvmtypes.InvalidRequest{Err: "buyStorage is null"}
