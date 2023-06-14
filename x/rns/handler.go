99c47671ab9e7d0737ed17693c7564cb9b1fba4d
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-06-14 13:32:20 -0400

    added update handler

--- x/rns/handler.go
+++ x/rns/handler.go
@@ -50,6 +50,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgInit:
 			res, err := msgServer.Init(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgUpdate:
+			res, err := msgServer.Update(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
