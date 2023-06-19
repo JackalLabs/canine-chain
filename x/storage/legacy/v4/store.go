555a194ed6ac077badfd92c45e4031d88b1d5dd8
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-06-19 16:14:36 -0400

    fix import cycle

--- x/storage/legacy/v4/store.go
+++ x/storage/legacy/v4/store.go
@@ -2,14 +2,19 @@ package v4
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/jackalLabs/canine-chain/x/storage/keeper"
+	"github.com/jackalLabs/canine-chain/x/storage/types"
 )
+
+type ParamGetterSetter interface {
+	GetParams(ctx sdk.Context) types.Params
+	SetParams(ctx sdk.Context, params types.Params)
+}
 
 // MigrateStore performs in-place store migrations from v1 to v2
 // The things done here are the following:
 // 1. setting up the next reason id and report id keys for existing subspaces
 // 2. setting up the module params
-func MigrateStore(ctx sdk.Context, k *keeper.Keeper) error {
+func MigrateStore(ctx sdk.Context, k ParamGetterSetter) error {
 	ctx.Logger().Error("Migrating Storage Store to V4!")
 	params := k.GetParams(ctx)
 
@@ -17,6 +22,7 @@ func MigrateStore(ctx sdk.Context, k *keeper.Keeper) error {
 	params.AttestMinToPass = 3
 
 	k.SetParams(ctx, params)
+
 	ctx.Logger().Info("DONE MIGRATING!")
 
 	return nil
--- x/storage/types/errors.go
+++ x/storage/types/errors.go
@@ -11,7 +11,6 @@ var (
 	ErrDivideByZero        = sdkerrors.Register(ModuleName, 1110, "DivideByZero")
 	ErrProviderNotFound    = sdkerrors.Register(ModuleName, 1111, "Provider not found. Please init your provider.")
 	ErrNotValidTotalSpace  = sdkerrors.Register(ModuleName, 1112, "Not a valid total space. Please enter total number of bytes to provide.")
-	ErrCannotVerifyProof   = sdkerrors.Register(ModuleName, 1113, "Cannot verify Proof")
 	ErrDealNotFound        = sdkerrors.Register(ModuleName, 1114, "Cannot find active deal")
 	ErrFormNotFound        = sdkerrors.Register(ModuleName, 1115, "Cannot find attestation form")
 	ErrAttestInvalid       = sdkerrors.Register(ModuleName, 1116, "Cannot attest to form")
