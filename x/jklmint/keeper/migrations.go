5f0fc9b9797f2399518307e01c7810c12e014923
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-07-05 12:12:26 -0400

    mint module adjustments

---
+++ x/jklmint/keeper/migrations.go
@@ -0,0 +1,25 @@
+package keeper
+
+// DONTCOVER
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v210 "github.com/jackalLabs/canine-chain/x/jklmint/legacy/v210"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	k Keeper
+}
+
+// NewMigrator returns a new Migrator
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{
+		k: keeper,
+	}
+}
+
+// Migrate1to2 migrates from version 2 to 3.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v210.MigrateStore(ctx, &m.k.paramSpace)
+}
