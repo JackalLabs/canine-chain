ae8c4062c2bc405eb97a98908eac977de0dfdcad
BiPhan4 <biphan374@gmail.com>
2023-09-29 14:47:57 -0400

    ibc hooks test: receive transfer with metadata

--- x/ibc-hooks/testing/ibc_hooks_test.go
+++ x/ibc-hooks/testing/ibc_hooks_test.go
@@ -2,9 +2,11 @@ package testing
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	ibctesting "github.com/cosmos/ibc-go/v4/testing"
+	"github.com/jackalLabs/canine-chain/v3/testutil"
 	"github.com/stretchr/testify/suite"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -124,4 +126,63 @@ func (suite *IBCHooksTestSuite) makeMockPacket(receiver, memo string, prevSequen
 		clienttypes.NewHeight(0, 100),
 		0,
 	)
+}
+
+func (suite *IBCHooksTestSuite) receivePacket(receiver, memo string) []byte {
+	return suite.receivePacketWithSequence(receiver, memo, 0)
+}
+
+func (suite *IBCHooksTestSuite) receivePacketWithSequence(receiver, memo string, prevSequence uint64) []byte {
+	channelCap := suite.chainB.GetChannelCapability(
+		suite.pathAB.EndpointB.ChannelConfig.PortID,
+		suite.pathAB.EndpointB.ChannelID)
+
+	packet := suite.makeMockPacket(receiver, memo, prevSequence)
+
+	err := suite.chainB.GetJackalApp().HooksICS4Wrapper.SendPacket(
+		suite.chainB.GetContext(), channelCap, packet)
+	suite.Require().NoError(err, "IBC send failed. Expected success. %s", err)
+
+	// Update both clients
+	err = suite.pathAB.EndpointB.UpdateClient()
+	suite.Require().NoError(err)
+	err = suite.pathAB.EndpointA.UpdateClient()
+	suite.Require().NoError(err)
+
+	// recv in chain a
+	res, err := suite.pathAB.EndpointA.RecvPacketWithResult(packet)
+	suite.Require().NoError(err)
+
+	// get the ack from the chain a's response
+	ack, err := ibctesting.ParseAckFromEvents(res.GetEvents())
+	suite.Require().NoError(err)
+
+	// manually send the acknowledgement to chain b
+	err = suite.pathAB.EndpointA.AcknowledgePacket(packet, ack)
+	suite.Require().NoError(err)
+	return ack
+}
+
+// // TO DO: unmarshal the acknowledgement. Not sure why it can't be unmarshalled at this time.
+func (suite *IBCHooksTestSuite) TestRecvTransferWithMetadata() {
+	logger, logFile := testutil.CreateLogger()
+	// Setup contract
+	suite.chainA.StoreContractCode(&suite.Suite, "./bytecode/echo.wasm")
+	addr := suite.chainA.InstantiateContract(&suite.Suite, "{}", 1)
+	logger.Printf("The contract address is %s:\n", addr)
+	ackBytes := suite.receivePacket(addr.String(), fmt.Sprintf(`{"wasm": {"contract": "%s", "msg": {"echo": {"msg": "test"} } } }`, addr))
+	logger.Printf("Acknowledgemenet bytes is %s:\n", ackBytes)
+	ackStr := string(ackBytes)
+	logger.Printf("Acknowledgemenet string is %s:\n", ackBytes)
+
+	fmt.Println(ackStr)
+
+	var ack map[string]string // This can't be unmarshalled to Acknowledgement because it's fetched from the events
+	err := json.Unmarshal(ackBytes, &ack)
+	fmt.Println(err)
+	suite.Require().NoError(err)
+	logFile.Close()
+	suite.Require().NotContains(ack, "error")
+	// TO DO: Implement our own acknowledgement string
+	// suite.Require().Equal(ack["result"], "eyJjb250cmFjdF9yZXN1bHQiOiJkR2hwY3lCemFHOTFiR1FnWldOb2J3PT0iLCJpYmNfYWNrIjoiZXlKeVpYTjFiSFFpT2lKQlVUMDlJbjA9In0=")
 }
