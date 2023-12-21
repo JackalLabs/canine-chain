efe88b05ab84849041f34d86927a509bbf1498ab
marston <marston@jackallabs.io>
2023-12-21 11:08:26 -0500

    lint

--- x/jklmint/types/account_names_test.go
+++ x/jklmint/types/account_names_test.go
@@ -1,9 +1,10 @@
 package types_test
 
 import (
+	"testing"
+
 	"github.com/jackalLabs/canine-chain/v3/x/jklmint/types"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestNames(t *testing.T) {
--- x/jklmint/utils/mint_test.go
+++ x/jklmint/utils/mint_test.go
@@ -1,9 +1,10 @@
 package utils_test
 
 import (
+	"testing"
+
 	"github.com/jackalLabs/canine-chain/v3/x/jklmint/utils"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestOwedTokens(t *testing.T) {
