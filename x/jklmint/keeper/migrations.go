c067c6192c4fea99502868854c27e6c019d82025
marston <marston@jackallabs.io>
2024-01-08 12:29:38 -0500

    fixing mint module params to allow decimals

--- x/jklmint/keeper/migrations.go
+++ x/jklmint/keeper/migrations.go
@@ -5,6 +5,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v210 "github.com/jackalLabs/canine-chain/v3/x/jklmint/legacy/v210"
+	v310 "github.com/jackalLabs/canine-chain/v3/x/jklmint/legacy/v310"
 )
 
 // Migrator is a struct for handling in-place store migrations.
@@ -19,7 +20,12 @@ func NewMigrator(keeper Keeper) Migrator {
 	}
 }
 
-// Migrate1to2 migrates from version 2 to 3.
+// Migrate2to3 migrates from version 2 to 3.
 func (m Migrator) Migrate2to3(ctx sdk.Context) error {
 	return v210.MigrateStore(ctx, &m.k.paramSpace)
+}
+
+// Migrate3to4 migrates from version 3 to 4.
+func (m Migrator) Migrate3to4(ctx sdk.Context) error {
+	return v310.MigrateStore(ctx, &m.k.paramSpace)
 }
--- x/jklmint/module.go
+++ x/jklmint/module.go
@@ -151,6 +151,11 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	if err != nil {
 		panic(err)
 	}
+
+	err = cfg.RegisterMigration(types.ModuleName, 3, m.Migrate3to4)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // RegisterInvariants registers the capability module's invariants.
@@ -175,7 +180,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 3 }
+func (AppModule) ConsensusVersion() uint64 { return 4 }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to the capability module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
