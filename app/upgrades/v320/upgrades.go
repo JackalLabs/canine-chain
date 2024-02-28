7d2879da9df31ef283153b5b4995fa003e8438eb
marston <marston@jackallabs.io>
2024-02-28 16:17:52 -0500

    better logging

--- app/upgrades/v320/upgrades.go
+++ app/upgrades/v320/upgrades.go
@@ -1,11 +1,13 @@
 package v320
 
 import (
+	"fmt"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/jackalLabs/canine-chain/v3/app/upgrades"
+	storagekeeper "github.com/jackalLabs/canine-chain/v3/x/storage/keeper"
 	storagemoduletypes "github.com/jackalLabs/canine-chain/v3/x/storage/types"
 )
 
@@ -15,6 +17,7 @@ var _ upgrades.Upgrade = &Upgrade{}
 type Upgrade struct {
 	mm           *module.Manager
 	configurator module.Configurator
+	keeper       storagekeeper.Keeper
 }
 
 // NewUpgrade returns a new Upgrade instance
@@ -34,6 +37,30 @@ func (u *Upgrade) Name() string {
 func (u *Upgrade) Handler() upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		fromVM[storagemoduletypes.ModuleName] = 5
+
+		ctx.Logger().Info("Upgrading the Jackal Network to:\n██╗   ██╗██████╗    ██████╗     ██████╗ \n██║   ██║╚════██╗   ╚════██╗   ██╔═████╗\n██║   ██║ █████╔╝    █████╔╝   ██║██╔██║\n╚██╗ ██╔╝ ╚═══██╗   ██╔═══╝    ████╔╝██║\n ╚████╔╝ ██████╔╝██╗███████╗██╗╚██████╔╝\n  ╚═══╝  ╚═════╝ ╚═╝╚══════╝╚═╝ ╚═════╝ ")
+
+		u.keeper.IterateLegacyActiveDeals(ctx, func(deal storagemoduletypes.LegacyActiveDeals) bool {
+
+			ctx.Logger().Info(fmt.Sprintf("%s being migrated", deal.Cid))
+
+			u.keeper.SetActiveDeals(ctx, storagemoduletypes.ActiveDeals{
+				Cid:          deal.Cid,
+				Signee:       deal.Signee,
+				Provider:     deal.Provider,
+				Startblock:   deal.Startblock,
+				Endblock:     deal.Endblock,
+				Filesize:     deal.Filesize,
+				LastProof:    ctx.BlockHeight(),
+				Proofsmissed: deal.Proofsmissed,
+				Blocktoprove: deal.Blocktoprove,
+				Creator:      deal.Creator,
+				Merkle:       deal.Merkle,
+				Fid:          deal.Fid,
+			})
+
+			return false
+		})
 
 		newVM, err := u.mm.RunMigrations(ctx, u.configurator, fromVM)
 		if err != nil {
--- x/storage/legacy/v320/store.go
+++ x/storage/legacy/v320/store.go
@@ -1,35 +1,12 @@
 package v320
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/jackalLabs/canine-chain/v3/x/storage/types"
 )
 
-type ActiveDealSetter interface {
-	SetActiveDeals(ctx sdk.Context, activeDeals types.ActiveDeals)
-}
-
-func MigrateStore(ctx sdk.Context, k ActiveDealSetter, sk sdk.StoreKey, codec codec.BinaryCodec, ps *paramstypes.Subspace) error {
-	IterateLegacyActiveDeals(ctx, sk, codec, func(deal types.LegacyActiveDeals) bool {
-		k.SetActiveDeals(ctx, types.ActiveDeals{
-			Cid:          deal.Cid,
-			Signee:       deal.Signee,
-			Provider:     deal.Provider,
-			Startblock:   deal.Startblock,
-			Endblock:     deal.Endblock,
-			Filesize:     deal.Filesize,
-			LastProof:    ctx.BlockHeight(),
-			Proofsmissed: deal.Proofsmissed,
-			Blocktoprove: deal.Blocktoprove,
-			Creator:      deal.Creator,
-			Merkle:       deal.Merkle,
-			Fid:          deal.Fid,
-		})
-
-		return false
-	})
+func MigrateStore(ctx sdk.Context, ps *paramstypes.Subspace) error {
 
 	var params types.Params
 	ps.GetParamSet(ctx, &params)
