87994d985e2cb32c46c33cdfc5d268cdc1b98f19
marston <marston@jackallabs.io>
2024-05-07 11:40:17 -0400

    iterate should improve memory usage mildly

--- x/storage/keeper/msg_server_claim_stray.go
+++ x/storage/keeper/msg_server_claim_stray.go
@@ -17,17 +17,21 @@ func (k msgServer) ClaimStray(goCtx context.Context, msg *types.MsgClaimStray) (
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "stray contract either no longer is stray, or has been removed by the user")
 	}
 
-	ls := k.ListFiles(ctx, stray.Fid)
-
 	provider, found := k.GetProviders(ctx, msg.ForAddress)
 	if !found {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "not a provider")
 	}
 
-	for _, l := range ls {
-		if l == provider.Ip {
-			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "cannot claim a stray you own.")
+	f := false
+	k.IterateActiveDeals(ctx, func(deal types.ActiveDeals) bool { // we iterate now, ideally it is not consensus breaking but should improve memory usage, this is way better in v4
+		if deal.Provider == provider.Address {
+			f = true
+			return true
 		}
+		return false
+	})
+	if f {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "cannot claim a stray you own.")
 	}
 
 	size := sdk.NewInt(int64(stray.Size()))
