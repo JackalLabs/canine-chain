daeac83443a559b8ef6a62e208aa1cb01ead8972
marston <marston@jackallabs.io>
2024-10-25 18:15:33 -0400

    lint

--- cmd/canined/utils.go
+++ cmd/canined/utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/jackalLabs/canine-chain/v4/x/storage/types"
 	"github.com/spf13/cobra"
 )
@@ -11,8 +12,7 @@ func AddressGenerationCommand() *cobra.Command {
 		Use:   "get-address [value]",
 		Short: "Generate an address from text, the same system used to generate protocol owned accounts.",
 		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-
+		RunE: func(_ *cobra.Command, args []string) error {
 			acc, err := types.GetAccount(args[0])
 			if err != nil {
 				return err
