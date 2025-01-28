a919147adbfba48671a3374f73fc6d997532b8ee
marston <marston@jackallabs.io>
2025-01-28 16:55:38 -0500

    adding swagger support

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,17 +2,23 @@
 
 set -eo pipefail
 
-mkdir -p ./docs/client
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+mkdir -p ./tmp-swagger-gen
+cd proto
+proto_dirs=$(find canine_chain ../x -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
-    -I "proto" \
-    "$query_file" \
-    --swagger_out=./docs/client \
-    --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+    buf generate --template buf.gen.swagger.yaml $query_file
   fi
 done
+
+cd ..
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+# all the individual swagger files need to be configured in `config.json` for merging
+swagger-combine ./docs/config.json -o ./docs/swagger-ui/swagger.yaml -f yaml --includeDefinitions true
+
+sed -E -i '' '/(items|properties): \*ref_[0-9]+/d' ./docs/swagger-ui/swagger.yaml
+# clean swagger files
+rm -rf ./tmp-swagger-gen
