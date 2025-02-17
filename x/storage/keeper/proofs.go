1f3c1d090726853c49bcd971328a1de38bd50ae5
Ahmed M. <ahmmoussa22@gmail.com>
2025-02-17 14:53:06 -0500

    lint

--- x/storage/keeper/proofs.go
+++ x/storage/keeper/proofs.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/jackalLabs/canine-chain/v4/x/storage/types"
