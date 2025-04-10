f2cdc99d79d6f3f2043b8cb5fbd70cd3a4ca9e7e
marston <marston@jackallabs.io>
2025-04-10 12:41:55 -0400

    updating stray claiming

--- x/storage/types/message_postproof.go
+++ x/storage/types/message_postproof.go
@@ -9,8 +9,10 @@ import (
 )
 
 const TypeMsgPostProof = "post_proof"
+const TypeMsgPostProofFor = "post_proof_for"
 
 var _ sdk.Msg = &MsgPostProof{}
+var _ sdk.Msg = &MsgPostProofFor{}
 
 func NewMsgPostProof(creator string, merkle []byte, owner string, start int64, item []byte, hashList []byte, toProve int64) *MsgPostProof {
 	return &MsgPostProof{
@@ -46,6 +48,52 @@ func (msg *MsgPostProof) GetSignBytes() []byte {
 }
 
 func (msg *MsgPostProof) ValidateBasic() error {
+	prefix, _, err := bech32.DecodeAndConvert(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	if prefix != AddressPrefix {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator prefix (%s)", fmt.Errorf("%s is not a valid prefix here. Expected `jkl`", prefix))
+	}
+
+	return nil
+}
+
+func NewMsgPostProofFor(creator string, merkle []byte, owner string, start int64, item []byte, hashList []byte, toProve int64, provider string) *MsgPostProofFor {
+	return &MsgPostProofFor{
+		Creator:  creator,
+		Item:     item,
+		HashList: hashList,
+		Merkle:   merkle,
+		Owner:    owner,
+		Start:    start,
+		ToProve:  toProve,
+		Provider: provider,
+	}
+}
+
+func (msg *MsgPostProofFor) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgPostProofFor) Type() string {
+	return TypeMsgPostProofFor
+}
+
+func (msg *MsgPostProofFor) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgPostProofFor) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgPostProofFor) ValidateBasic() error {
 	prefix, _, err := bech32.DecodeAndConvert(msg.Creator)
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
